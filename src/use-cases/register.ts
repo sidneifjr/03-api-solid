0f13923815d3ebe3295d5fcecd7444c44c11f03a
Sidnei Farias <sfarias.dev@gmail.com>
2026-07-22 00:05:31 -0300

    refactor(use-cases): implements dependency inversion in register controller

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -8,32 +7,36 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-// Separar este código em um use-case é importante pois, atualmente, estamos registrando um usuário via rota HTTP.
-// Ao desacoplar esse código, ele será reutilizável em outras contextos; assim, podemos registrar um usuário de outras formas.
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  // 6 rounds de geração de hash contínuo (um hash é gerado a partir da senha e, a partir desse hash, um novo hash é gerado - repete 6 vezes).
-  // Em aplicações de muito tráfego, essa quantidade de rounds causará um consumo alto de processamento; portanto, não é apropriado para esse cenário.
-  const password_hash = await hash(password, 6)
+// SOLID
+// D -> Inversion Dependency Principle
+// Ao invés da minha classe instanciar as dependências que ela precisa, ela vai receber as dependências como parâmetro.
+export class RegisterUseCase {
+  constructor(private usersRepository: any) { }
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+  // Separar este código em um use-case é importante pois, atualmente, estamos registrando um usuário via rota HTTP.
+  // Ao desacoplar esse código, ele será reutilizável em outras contextos; assim, podemos registrar um usuário de outras formas.
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    // 6 rounds de geração de hash contínuo (um hash é gerado a partir da senha e, a partir desse hash, um novo hash é gerado - repete 6 vezes).
+    // Em aplicações de muito tráfego, essa quantidade de rounds causará um consumo alto de processamento; portanto, não é apropriado para esse cenário.
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    // const prismaUsersRepository = new PrismaUsersRepository()
+
+    // Graças à implementação do DIP, não há mais dependência direto do Prisma.
+    await this.usersRepository.create({
+      name,
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
